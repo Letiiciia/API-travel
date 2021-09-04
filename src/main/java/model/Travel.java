00c0eeedd40a2ab759403ab048c554532a93c943
Letícia Lima <leticialimaferreira2@gmail.com>
2021-09-04 09:06:37 -0300

    Model created with Static and Travel

---
+++ src/main/java/model/Travel.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import sun.awt.image.ImageWatched.Link;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Travel {
+	
+	private Long id;
+	private String orderNumber;
+	private LocalDateTime startDate;
+	private LocalDateTime endDate;
+	private BigDecimal amount;
+	private TravelTypeEnum type;
+	private List<Link> links;
+	
+	public Travel(TravelTypeEnum type){
+		this.type = type;
+	}
+	
+	public void addLink(Link link) {
+		if(links == null) links = new ArrayList<>();
+		links.add(link);
+	}
+}
