ad8195622c79f1f4ec37e0d8da3febb1f12b3b87
Letícia Lima <leticialimaferreira2@gmail.com>
2021-09-04 10:08:22 -0300

    travelService implemented with method of creation

--- src/main/java/service/TravelService.java
+++ src/main/java/service/TravelService.java
@@ -1,5 +1,17 @@
 package service;
 
-public class TravelService {
+import org.springframework.stereotype.Service;
 
+@Service
+public class TravelService {
+	private TravelFactory factory;
+	private List<Travel> travels;
+
+	public void createTravelFactory() {
+		if (factory == null) {
+			factory = new TravelFactoryImpl();
+		}
+	}
+	
+	
 }
